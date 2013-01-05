3b0999d90ec94abef6122e32f768e7e2b2bdf21e
Paul Brannan <cout@rubystuff.org>
2013-01-05 13:43:11 -0500

    Initial add

---
+++ plugins/reload.rb
@@ -0,0 +1,31 @@
+class ReloadPlugin
+  include Cinch::Plugin
+
+  match /reload/
+
+  def execute(m)
+    reload_all_plugins
+  end
+
+  def reload_all_plugins
+    bot.plugins.each do |plugin|
+      reload_plugin(plugin)
+    end
+  end
+
+  def reload_plugin(plugin)
+    begin
+      file, line = plugin.method(:execute).source_location
+      return if not File.exists?(file)
+      bot.plugins.unregister_plugin(plugin)
+      begin
+        load "#{file}"
+      ensure
+        bot.plugins.register_plugin(plugin.class)
+      end
+    rescue Exception => e
+      bot.loggers.error "Unable to reload #{plugin}: #{e}"
+    end
+  end
+end
+
---
+++ u-2002.rb
@@ -0,0 +1,16 @@
+require 'cinch'
+require './plugins/reload'
+
+bot = Cinch::Bot.new do
+  configure do |c|
+    # c.server = 'irc.freenode.org'
+    c.server = 'localhost'
+    c.nick = 'U-2002'
+    c.channels = [ '#alt-255' ]
+    c.plugins.plugins = [ ReloadPlugin ]
+    c.plugins.prefix = '.'
+  end
+end
+
+bot.start
+
