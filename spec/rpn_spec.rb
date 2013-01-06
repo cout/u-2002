44d59761d703218c168d94fd3ee2cfbb8f1feb17
Paul Brannan <cout@rubystuff.org>
2013-01-06 08:25:28 -0500

    Added spec for rpn plugin

---
+++ spec/rpn_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+require 'mock_bot'
+require './plugins/rpn'
+
+describe Plugins::Rpn do
+  let!(:bot) { MockBot.new }
+  let!(:plugin) { Plugins::Rpn.new(bot) }
+  let!(:matcher) { Plugins::Rpn.matchers[0] }
+  let!(:message) { double() }
+
+  describe :match do
+    it 'should give the whole expression' do
+      match = matcher.pattern.match "rpn 1"
+      match[0].should eq 'rpn 1'
+      match[1].should eq '1'
+    end
+
+    it 'should give the whole expression even when there are spaces' do
+      match = matcher.pattern.match "rpn 1 1 +"
+      match[0].should eq 'rpn 1 1 +'
+      match[1].should eq '1 1 +'
+    end
+  end
+
+  describe :execute do
+    it 'should evaluate the expression' do
+      expect_reply 'Result: 2'
+      plugin.execute(message, '1 1 +')
+    end
+  end
+end
