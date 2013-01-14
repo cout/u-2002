9e505b776636dd789c6ef1eeffa7e795aab69b90
Paul Brannan <cout@rubystuff.org>
2013-01-14 09:58:34 -0500

    Added locations database

--- plugins/weather.rb
+++ plugins/weather.rb
@@ -7,22 +7,91 @@ module Plugins
 class Weather
   include Cinch::Plugin
 
-  match /weather (.*)/,
+  match /weather\s*(.*)/,
         method: :weather,
         help: "Tells the current weather for the given location"
 
-  match /forecast (.*)/,
+  match /forecast\s*(.*)/,
         method: :forecast,
         help: "Tells the forecast for the given location"
 
+  match /i live in (.*)/i,
+        method: :set_location,
+        help: "Set's your location"
+
+  match /where do i live/i,
+        method: :show_my_location,
+        help: "Set's your location"
+
+  match /where does (.*?) live/i,
+        method: :show_location,
+        help: "Set's your location"
+
   set help: "weather <where> - tells the current weather for the given location\n" + \
-            "forecast <where> - tells the forecast for the given location"
+            "forecast <where> - tells the forecast for the given location\n" + \
+            "I live in <where> - sets your location\n" + \
+            "Where do I live / Where does <who> live - tells a user's location"
+
+  def initialize(*args)
+    super(*args)
+    load_locations
+  end
+
+  WEATHER_LOCATIONS = 'weather_locations.yaml'
+
+  def load_locations
+    if File.exists?(WEATHER_LOCATIONS) then
+      @locations = YAML.load_file(WEATHER_LOCATIONS)
+    else
+      @locations = { }
+    end
+  end
+
+  def save_locations
+    synchronize(:update) do
+      File.open(WEATHER_LOCATIONS, 'w') do |out|
+        YAML.dump(@locations, out)
+      end
+    end
+  end
+
+  def set_location(m, where)
+    @locations[m.user.nick] = where
+    save_locations
+    m.reply("You now live in #{where}")
+  end
+
+  def show_location(m, who)
+    if @locations[who] then
+      m.reply("#{who} lives in #{@locations[who]}")
+    else
+      m.reply("I don't know where #{who} lives")
+    end
+  end
+
+  def show_my_location(m)
+    if @locations[m.user.nick] then
+      m.reply("You live in #{@locations[m.user.nick]}")
+    else
+      m.reply("I don't know where you live")
+    end
+  end
+
+  def find_location(where)
+    if @locations[where] then
+      where = @locations[where]
+    elsif where == '' and @locations[m.user.nick] then
+      where = @locations[m.user.nick]
+    end
+  end
 
   def weather(m, where)
+    where = find_location(where)
     m.reply(get_weather(where))
   end
 
   def forecast(m, where)
+    where = find_location(where)
     m.reply(get_forecast(where))
   end
 
